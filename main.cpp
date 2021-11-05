acbbb6e892027ab8f3c2637cee0dd520cf6f5bd1
jubuyer <jahmed10@stuy.edu>
2021-11-04 21:44:59 -0400

    minor changes

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main()
 {
   std::cout << "This is the main program.\n";
   std::cout << "To see tests please use the Makefile to run tests.cpp.\n";
-  std::cout << "The categories of tests are: Basic, Short, and Plaintext";
-  std::cout << "Example: solve(\"Ocdn dn ojj ncjmo, ndm!\") == \"This is too "
+  std::cout << "The categories of tests are: Basic, Short, and Plaintext\n";
+  std::cout << "Ex. solve(\"Ocdn dn ojj ncjmo, ndm!\") == \"This is too "
   "short, sir!\"";
   solve("Ocdn dn ojj ncjmo, ndm!") == "This is too short, sir!" ?
     std::cout << " -> Success!\n" : std::cout << "-> Failure!\n";
